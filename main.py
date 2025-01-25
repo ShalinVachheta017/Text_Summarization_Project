6adb524ccf15fa3f04da659ee2422f44da39c4a5
Vachheta <g063292@hpc-login02.cm.cluster>
2025-01-26 00:01:53 +0100

    till data ingestion

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from textSummarizer.logging import logger
 
-
-logger.info("we did it")
+STAGE_01_DATA_INGESTION = "stage_01_data_ingestion"
+try:
+    logger.info(f">>>>>>>>>>>Pipeline for {STAGE_01_DATA_INGESTION} started<<<<<<<")
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f">>>>>>>>>>>Pipeline for {STAGE_01_DATA_INGESTION} completed<<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
