54194c0ef87b8c8210e9ffda1e666064de852f86
Shalin Vachheta <shalinvachheta2016@gmail.com>
2025-03-12 16:30:43 +0100

    evaluation will be running soon

--- bash_cpu.sh
+++ bash_cpu.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --time=5-00:00:00
-#SBATCH --nodes=4
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=long
 #SBATCH --output=output.log
 #SBATCH --error=error.log
 
-Conda activate TextSummarization
 
-srun /work/ws-tmp/g063292-restore/g063292-projects-1736208608/Text _Summarization/Text_Summarization_Project/main.py
+# Activate Conda environment
+source ~/miniconda3/etc/profile.d/conda.sh  # Ensure Conda is available
+conda activate TestSmrz
+
+# Run the script
+srun python "/work/ws-tmp/g063292-restore/g063292-projects-1736208608/Text_Summarization/Text_Summarization_Project/main.py"
---
+++ src/textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,18 @@
+from textSummarizer.config.configuration import ConfigurationManager
+    
+from textSummarizer.components.model_evaluation import ModelEvaluation
+from textSummarizer.logging import logger
+import evaluate
+
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
